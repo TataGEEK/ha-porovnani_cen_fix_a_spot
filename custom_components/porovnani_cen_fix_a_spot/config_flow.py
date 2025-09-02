131eade01b234195772b4853666aa67432f4f1fa
Táta GEEK <63705187+TataGEEK@users.noreply.github.com>
2025-09-02 19:59:50 +0200

    doplněn entity selector pro switch

--- custom_components/porovnani_cen_fix_a_spot/config_flow.py
+++ custom_components/porovnani_cen_fix_a_spot/config_flow.py
@@ -4,32 +4,28 @@ import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.const import CONF_ENTITY_ID
 from homeassistant.core import callback
+from homeassistant.helpers import selector
 
 from .const import DOMAIN, DEFAULT_SOURCE_ENTITY_ID
 
-
-DATA_SCHEMA = vol.Schema(
-    {vol.Required(CONF_ENTITY_ID, default=DEFAULT_SOURCE_ENTITY_ID): str}
-)
-
+DATA_SCHEMA = vol.Schema({
+    vol.Required(CONF_ENTITY_ID, default=DEFAULT_SOURCE_ENTITY_ID):
+        selector.EntitySelector(
+            selector.EntitySelectorConfig(domain=["switch"])  # jen přepínače
+        )
+})
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        errors: dict[str, str] = {}
         if user_input is not None:
-            entity_id = user_input.get(CONF_ENTITY_ID)
-            if entity_id and entity_id.startswith("switch."):
-                return self.async_create_entry(
-                    title="Porovnání cen (HDO)",
-                    data={"source_entity_id": entity_id},
-                )
-            errors["base"] = "invalid_entity"
-
-        return self.async_show_form(
-            step_id="user", data_schema=DATA_SCHEMA, errors=errors
-        )
+            # selector už zajistí, že jde o switch.* – není nutné ručně kontrolovat
+            return self.async_create_entry(
+                title="Porovnání cen (HDO)",
+                data={"source_entity_id": user_input[CONF_ENTITY_ID]},
+            )
+        return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA)
 
     @staticmethod
     @callback
@@ -55,5 +51,11 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
             "source_entity_id",
             self.config_entry.data.get("source_entity_id", DEFAULT_SOURCE_ENTITY_ID),
         )
-        schema = vol.Schema({vol.Required(CONF_ENTITY_ID, default=current): str})
+
+        schema = vol.Schema({
+            vol.Required(CONF_ENTITY_ID, default=current):
+                selector.EntitySelector(
+                    selector.EntitySelectorConfig(domain=["switch"])
+                )
+        })
         return self.async_show_form(step_id="user", data_schema=schema)
